cf39fd51fe7b729a06e8d67ff4f18d9c5646e831
Hidde Boomsma <hidde@hostnet.nl>
2013-10-08 16:19:46 +0200

    removed garbage in output

--- src/loader.php
+++ src/loader.php
@@ -1,7 +1,6 @@
-#!/usr/bin/env php
 <?php
 
-require_once "__DIR__/../vendor/autoload.php";
+require_once __DIR__ .'/../vendor/autoload.php';
 
 if (PHP_SAPI == "cli") {
     $time_start = microtime(true);
