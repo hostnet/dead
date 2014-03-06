37e83ad29492b5cc28973406519e9dd41feb61dd
Hidde Boomsma <hidde@hostnet.nl>
2014-03-06 15:32:31 +0100

    make also loadable as composer package

--- src/loader.php
+++ src/loader.php
@@ -1,6 +1,10 @@
 <?php
 
-require_once __DIR__ .'/../vendor/autoload.php';
+if (file_exists(__DIR__ .'/../vendor/autoload.php')) {
+    require_once __DIR__ .'/../vendor/autoload.php';
+} else {
+    require_once __DIR__ .'/../../../autoload.php';
+}
 
 if (PHP_SAPI == "cli") {
     $time_start = microtime(true);
