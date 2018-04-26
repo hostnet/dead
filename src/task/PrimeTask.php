9a975d24a3311207b51cdf99d5228a9ae434ba67
Stijn Veken <sveken@hostnet.nl>
2018-04-26 09:28:08 +0200

    fixed line exceding max width

--- src/task/PrimeTask.php
+++ src/task/PrimeTask.php
@@ -256,7 +256,8 @@ class PrimeTask extends AbstractPdoTaskInterface
         $db    = $this->getDb();
 
         foreach ($dead_functions as $file_function) {
-            $query_string = "UPDATE $table SET deleted_at = NOW() WHERE deleted_at IS NULL AND function = :file_function";
+            $query_string =
+                "UPDATE $table SET deleted_at = NOW() WHERE deleted_at IS NULL AND function = :file_function";
             $query        = $db->prepare($query_string);
             $query->bindParam(":file_function", $file_function);
             $query->execute();
